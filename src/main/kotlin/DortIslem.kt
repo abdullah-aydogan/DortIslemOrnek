c3636662e184fb777b45244096f57c88a26dc1f9
Abdullah Aydoğan <aydoganabdullah34@gmail.com>
2023-03-13 16:39:48 +0300

    if else yapısı oluşturuldu

---
+++ src/main/kotlin/DortIslem.kt
@@ -0,0 +1,41 @@
+import java.util.Scanner
+
+fun main() {
+
+    val girdi = Scanner(System.`in`) // Scanner sınıfından nesne
+
+    val sayi1 = 0 // Sayı 1 tanımlama
+    val sayi2 = 0 // Sayı 2 tanımlama
+
+    println("Toplama (1)")
+    println("Çıkarma (2)")
+    println("Çarpma (3)")
+    println("Bölme (4)")
+
+    val secim = girdi.nextInt()
+
+    if (secim == 1) {
+
+        
+    }
+
+    else if (secim == 2) {
+
+
+    }
+
+    else if (secim == 3) {
+
+
+    }
+
+    else if (secim == 4) {
+
+
+    }
+
+    else {
+
+        println("Böyle bir işlem bulunmamaktadır.")
+    }
+}
